61d0ee2ebe2f704d191074b354d308e99e27739f
HossanaRanto <mahefaniainaranto@gmail.com>
2025-01-14 10:58:41 +0300

    add route to the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
