cd5e13b3196e7f0003c24fd1a423be34fa16490c
HossanaRanto <mahefaniainaranto@gmail.com>
2025-01-14 10:58:26 +0300

    remove unused css and add router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  
-  templateUrl: './app.component.html', 
-  styleUrls: ['./app.component.css']  
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'project-management-frontend';
