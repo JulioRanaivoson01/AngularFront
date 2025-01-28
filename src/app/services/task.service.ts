ddc4b3b9e49a1db2b5aafce1cd96874a74938058
HossanaRanto <mahefaniainaranto@gmail.com>
2025-01-28 11:30:51 +0300

    update url

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,31 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../models/task.model';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private baseUrl = 'http://localhost:9090/api/user/register';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(`${this.baseUrl}`);
+    return this.http.get<Task[]>(`${this.baseUrl}/tasks`);
   }
 
 
   createTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(`${this.baseUrl}`, task);
+    return this.http.post<Task>(`${this.baseUrl}/create-task`, task);
   }
 
 
   updateTask(id: string, task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.baseUrl}/${id}`, task);
+    return this.http.put<Task>(`${this.baseUrl}/task/${id}`, task);
   }
 
   deleteTask(id: string): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${this.baseUrl}/task/${id}`);
   }
 }
